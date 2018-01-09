f409cedf3bcecc5fb14a293bb0d7c134b0fcb053
Qi Wu <Michaelwoo@QimatoMacBook-Air.local>
2018-01-08 20:33:14 -0800

    add dynamic contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+	root 'static_pages#home'
 	#rule maps requests for the URL /static_pages/home to the home action in the Static Pages controller
   get 'static_pages/home'
   get 'static_pages/help'
 	get  'static_pages/about'
-  root 'application#hello'
+  
 end
