466183b00a0740b326369a739b157562493f91a0
Qi Wu <Michaelwoo@QimatoMacBook-Air.local>
2018-01-08 20:20:39 -0800

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+	#rule maps requests for the URL /static_pages/home to the home action in the Static Pages controller
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+	get  'static_pages/about'
   root 'application#hello'
 end
